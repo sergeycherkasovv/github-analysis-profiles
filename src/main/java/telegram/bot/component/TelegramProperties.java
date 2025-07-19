589a860198d3cf9ce70fe6fa3f24f9b196125a16
Sergey Cherkasov <iamcherkasov.job@gmail.com>
2025-07-19 17:36:54 +0700

    changed TelegramProperties.java

--- src/main/java/telegram/bot/component/TelegramProperties.java
+++ src/main/java/telegram/bot/component/TelegramProperties.java
@@ -1,5 +1,6 @@
-package telegram.bot.properties;
+package telegram.bot.component;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -10,5 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "telegram")
 public class TelegramProperties {
+    @NotBlank
     private String token;
 }
