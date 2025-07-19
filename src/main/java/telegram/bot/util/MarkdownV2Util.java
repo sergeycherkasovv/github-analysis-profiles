b840fe9c5e89f864a118e813a33e2ca5c16d79a3
Sergey Cherkasov <iamcherkasov.job@gmail.com>
2025-07-19 17:34:55 +0700

    created the MarkdownV2Util.java class for escaping characters

---
+++ src/main/java/telegram/bot/util/MarkdownV2Util.java
@@ -0,0 +1,31 @@
+package telegram.bot.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarkdownV2Util {
+    public String escapeMarkdownV2(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+
+        return text.replace("_", "\\_")
+                .replace("*", "\\*")
+                .replace("[", "\\[")
+                .replace("]", "\\]")
+                .replace("(", "\\(")
+                .replace(")", "\\)")
+                .replace("~", "\\~")
+                .replace("`", "\\`")
+                .replace(">", "\\>")
+                .replace("#", "\\#")
+                .replace("+", "\\+")
+                .replace("-", "\\-")
+                .replace("=", "\\=")
+                .replace("|", "\\|")
+                .replace("{", "\\{")
+                .replace("}", "\\}")
+                .replace(".", "\\.")
+                .replace("!", "\\!");
+    }
+}
