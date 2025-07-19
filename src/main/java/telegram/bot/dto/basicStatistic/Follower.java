4fa87bddfaa353488676c5b3e9f24dfb83b8b397
Sergey Cherkasov <iamcherkasov.job@gmail.com>
2025-07-19 17:31:07 +0700

    created a dto entity Follower.java

---
+++ src/main/java/telegram/bot/dto/basicStatistic/Follower.java
@@ -0,0 +1,11 @@
+package telegram.bot.dto.basicStatistic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Follower {
+    private Integer followers;
+    private Integer following;
+}
