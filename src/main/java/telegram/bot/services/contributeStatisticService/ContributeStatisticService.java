f6de1af491baea92729d6766d68cfdfe49d65ca1
Sergey Cherkasov <iamcherkasov.job@gmail.com>
2025-07-19 17:29:14 +0700

    created a class for handling the ContributionStatistics

---
+++ src/main/java/telegram/bot/services/contributeStatisticService/ContributeStatisticService.java
@@ -0,0 +1,21 @@
+package telegram.bot.services.contributeStatisticService;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.stereotype.Service;
+import telegram.bot.dto.contributeStatistic.ContributeStatistic;
+
+@Service
+public class ContributeStatisticService {
+    public ContributeStatistic createContributeStatistic(JsonNode username) {
+        var contribute = new ContributeStatistic();
+
+        JsonNode data = username.path("data").path("user").path("contributionsCollection");
+
+        contribute.setTotalCommits(data.path("totalCommitContributions").asLong(0));
+        contribute.setPullRequest(data.path("totalPullRequestContributions").asLong(0));
+        contribute.setCodeReviews(data.path("totalPullRequestReviewContributions").asLong(0));
+        contribute.setIssues(data.path("totalIssueContributions").asLong(0));
+
+        return contribute;
+    }
+}
