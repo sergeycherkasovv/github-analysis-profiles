642ec013cef9fd3960b5beb70d4c5601aa8b7fda
Sergey Cherkasov <iamcherkasov.job@gmail.com>
2025-07-20 17:54:55 +0700

    created GitHubServiceTest.java

---
+++ src/test/java/telegram/bot/services/GitHubServiceTest.java
@@ -0,0 +1,14 @@
+package telegram.bot.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class GitHubServiceTest {
+    @Test
+    void fetchBasicStatistic() {
+        assertTrue(true);
+    }
+}
