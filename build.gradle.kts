adf2ca18e7a8e6aa0b73fad9148a5bd9c23e5c8b
Sergey Cherkasov <iamcherkasov.job@gmail.com>
2025-07-25 20:30:51 +0700

    deleted implements to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,9 +41,7 @@ dependencies {
 	// Tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation(platform("org.junit:junit-bom:5.11.2"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
 }
 
 tasks.test {
