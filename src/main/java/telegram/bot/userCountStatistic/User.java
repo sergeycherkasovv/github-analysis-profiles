84602dcac8b7c0129f3743436d8b2a3eadd1daa6
Sergey Cherkasov <iamcherkasov.job@gmail.com>
2025-07-27 23:07:31 +0700

    changed User.java

--- src/main/java/telegram/bot/userCountStatistic/User.java
+++ src/main/java/telegram/bot/userCountStatistic/User.java
@@ -1,6 +1,11 @@
 package telegram.bot.userCountStatistic;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
